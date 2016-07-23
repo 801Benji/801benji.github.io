ec89c8ca3177f6849b7935e0e983ea1339a52e99
Benjamin Medrano <801benji@gmail.com>
2016-07-23 21:18:01 +0000

    To do list update

--- main.js
+++ main.js
@@ -8,12 +8,23 @@ $(document).ready(function(){
         alert("Not so great!");
     }
 });
+
+
+$('textarea').focus(); 
 $('#button1').on('click', function(){
  var list = $("#text").val();
 
- $('table').append(list);
+var div =  $('table').append(list);
+    $(div).append("<div></div>");
 
+    
 });
+  
+
+
+
+
+
     function math(a) {
  var a = prompt("What do you wan to multiply first?");
  var b = prompt("What do you want to multiply second?");
