e4208ddaef55ce385b83f81e429545fd2c09eb02
Benjamin Medrano <801benji@gmail.com>
2016-08-02 05:04:43 +0000

    Great Update

---
+++ click.js
@@ -0,0 +1,21 @@
+/* global $ */
+$(document).ready(function(){
+    alert("Welcome");
+});
+$('#bgchange').on('click', function(){
+    $('body').toggleClass('flick')
+});
+
+$('#boxmaker').on('click', function(){
+    var maker = $('table').append('<div> </div>');
+  var boxcounter = 0;
+    for(var i = 0; i <= 'div'; i++){
+        boxcounter++;
+         alert("You have " + boxcounter + " boxes");
+           return maker;
+    }
+    });
+$('#rotatebox').on('click', function(){
+ $("table").transition({ x: '-200%',  opacity: 0.3 });
+});
+
